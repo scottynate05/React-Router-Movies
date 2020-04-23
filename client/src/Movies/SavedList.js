722eab8c92cf2650c472e03395607df2816892d0
Nathan Brown <scottynate05@yahoo.com>
2020-04-22 21:24:19 -0700

    mvp

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
-
+function SavedList(props) {
+  // const { goBack } = useHistory();
+  return (  
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <NavLink to="/">
+        <div className="home-button">Home</div>
+      </NavLink>
+    </div>
+  );
+}
 export default SavedList;
